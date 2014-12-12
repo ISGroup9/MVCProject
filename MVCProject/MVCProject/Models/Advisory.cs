ef2ba2e5ded92f59c38696b00791d5b3bb7d9abe
johnmeservy <meservy@gmail.com>
2014-12-11 22:02:04 -0700

    Please don't make changes yet

---
+++ MVCProject/MVCProject/Models/Advisory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Advisory
+    {
+       
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+
+        public string Picture { get; set; }
+    }
+
+    public class AdvisoryDbContext : DbContext
+    {
+        public DbSet<Advisory> Advisory { get; set; }
+    }
+}
---
+++ MVCProject/MVCProject/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string GradPlan { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+    public class ContactDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
